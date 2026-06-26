47ece3103b1da195496e3611ed392a4bb5f91ede
rishabhka <77171218+rishabhka@users.noreply.github.com>
2026-06-26 18:16:07 +0530

    Add: coin-change - submission-12

---
+++ Data Structures & Algorithms/coin-change/submission-12.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minCoin(int [] coins,int i,int amount)
+    {
+        if(amount == 0)
+        return 0;
+        else if(i>=coins.length)
+        return Integer.MAX_VALUE/2;
+        else
+        {
+            if(amount-coins[i]<0)
+            return minCoin(coins,i+1,amount);
+            return Math.min(minCoin(coins,i,amount-coins[i])+1,
+            minCoin(coins,i+1,amount));
+        }
+    }
+    public int coinChange(int[] coins, int amount) {
+        if(amount ==0 || coins.length==0)
+        return 0;
+        int result = minCoin(coins,0,amount);
+        return result==Integer.MAX_VALUE/2?-1:result;
+    }
+}
