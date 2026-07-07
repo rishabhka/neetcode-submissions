2b4f5f31cb4d7e95a8493caab9521e83a6199ace
rishabhka <77171218+rishabhka@users.noreply.github.com>
2026-07-07 23:38:04 +0530

    Add: perfect-squares - submission-6

---
+++ Data Structures & Algorithms/perfect-squares/submission-6.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int f(int n,int [] dp)
+    {
+        if(n==0||n==1)
+        return n;
+        else if(n<0) return Integer.MAX_VALUE/2;
+        else if (dp[n]!=-1) return dp[n];
+        else
+        {
+            int min = Integer.MAX_VALUE/2;
+            for(int i=1;i*i<=n;i++)
+            {
+                min = Math.min(f(n-(int)Math.pow(i,2),dp)+1,min);
+            }
+            dp[n]= min;
+            return dp[n];
+        }
+    }
+    public int numSquares(int n) {
+        int [] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return f(n,dp);
+    }
+}
