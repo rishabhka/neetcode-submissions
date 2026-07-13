af4fdc6ed39dbbdb85f3913851dbed33dd507571
rishabhka <77171218+rishabhka@users.noreply.github.com>
2026-07-13 09:41:35 +0530

    Add: course-schedule-iv - submission-6

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-6.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean dfs(List<List<Integer>> adjList,int src,int dst,int [] visited)
+    {
+        if(src==dst) return true ;
+        visited[src] = 1;
+        for(int neigh : adjList.get(src) )
+        {
+            if(visited[neigh]==1)
+            continue ;
+            if(dfs(adjList,neigh,dst,visited))
+            return true ;
+        }
+        return false;
+
+    }
+    public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
+        List<Boolean> res = new ArrayList<>();
+        List<List<Integer>> adjList = new ArrayList<>();
+        for(int i=0;i<numCourses;i++)
+        adjList.add(new ArrayList<>());
+
+        for(int i=0;i<prerequisites.length;i++)
+        {
+            adjList.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+        for(int i=0;i<queries.length;i++)
+        {
+            int [] visited = new int [numCourses] ;
+            if(dfs(adjList,queries[i][0],queries[i][1],visited))
+            res.add(true);
+            else 
+            res.add(false);
+        }
+        return res ;
+    }
+}
