f6b022d2055c8e6d8f54044b49c7b24b350923eb
rishabhka <77171218+rishabhka@users.noreply.github.com>
2026-07-14 13:30:40 +0530

    Add: meeting-rooms-iii - submission-1

---
+++ Data Structures & Algorithms/meeting-rooms-iii/submission-1.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int mostBooked(int n, int[][] meetings) {
+        PriorityQueue<long[]> pq = new PriorityQueue<>(
+            (a,b) ->
+            {
+                return Long.compare(a[0],b[0]);
+            }
+        );
+        int [] count = new int [n] ;
+        long [][] occupancy = new long [n][2] ;
+        for(int i=0;i<meetings.length;i++)
+        {
+            pq.add(new long[]{meetings[i][0],meetings[i][1]});
+        }
+        while(!pq.isEmpty())
+        {
+            long [] node = pq.poll(); boolean isInsert = false ;
+            for(int i=0;i<n;i++)
+            {
+                if(occupancy[i][1]<=node[0]) // insert it
+                {
+                    occupancy[i] = node ;
+                    isInsert = true ;
+                    count[i] ++ ;
+                    break ;
+                }
+            }
+            if(!isInsert) // if no insert then insert to min idx , which is freed earliest
+            {
+                int i =0 ; // to store room which is closest
+                long max = Integer.MAX_VALUE ;
+                for(int k=0;k<n;k++) // to find min last value
+                {
+                    if(occupancy[k][1]<max)
+                    {
+                        max = occupancy[k][1];
+                        i = k;
+                    }
+                }
+                long diff = node[1]-node[0] ;
+                node[0] = occupancy[i][1] ;
+                node[1] = occupancy[i][1] + diff ;
+                occupancy[i] = node;
+                count[i] ++ ;
+            }
+
+        } int idx = -1; long max =0;
+        for(int i=0;i<n;i++)
+        {
+        if(count[i]>max)
+        {
+        max = count[i] ;
+        idx = i;
+        }
+        }
+        return idx;
+    }
+}
