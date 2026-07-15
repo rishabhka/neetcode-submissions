0f58aee5384746e3381cdb5641b03c5e5a8d393d
rishabhka <77171218+rishabhka@users.noreply.github.com>
2026-07-15 10:03:29 +0530

    Add: dota2-senate - submission-3

---
+++ Data Structures & Algorithms/dota2-senate/submission-3.java
@@ -0,0 +1,53 @@
+class Solution {
+    public String predictPartyVictory(String s) {
+        int p =0 ;int q=0 ; int r =0 ;
+        int [] nums = new int [s.length()] ;
+        for(int i=0;i<nums.length;i++)
+        nums[i] = 1;
+        while(q<s.length())
+        {
+            if(s.charAt(p)==s.charAt(q))
+            {
+                if(s.charAt(r)!=s.charAt(q))
+                {
+                    nums[r] = -1;
+                    r++;
+                }
+            }
+            else
+            {
+                if(nums[p]==1)
+                {
+                    nums[q] = -1;
+                    p++;
+                }
+            }
+            q++;
+        } int count =0; boolean flag = true ; char first='a' ;
+        for(int i=0;i<s.length();i++)
+        {   
+        if(s.charAt(i)=='D'&&nums[i]==1) 
+        {count ++;
+        if(flag)
+        {
+            flag = false ;
+            first = s.charAt(i);
+        }
+        }
+        else if(s.charAt(i)=='R'&&nums[i]==1)
+        {count -- ;
+        if(flag)
+        {
+            flag = false ;
+            first = s.charAt(i);
+        }
+        }
+        }
+        System.out.println(count);
+        if(count==0){
+            if(first=='D') return "Dire";
+            else return "Radiant" ;
+        }
+        return count>0?"Dire":"Radiant" ;
+    }
+}
