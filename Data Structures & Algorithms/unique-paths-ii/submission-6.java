69b44cf2f1072c175ae3cdc470ad607babe609c8
rishabhka <77171218+rishabhka@users.noreply.github.com>
2026-08-01 09:44:21 +0530

    Add: unique-paths-ii - submission-6

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-6.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] grid) {
+        int m = grid.length ;
+        int n = grid[0].length ; boolean flag = false ;
+        if(grid[0][0]==1) return 0;
+        for(int i=0;i<m;i++)
+        {
+        if(grid[i][0]==1||flag) { grid[i][0] = -1 ; flag = true ; }
+        else grid[i][0] = 1 ;
+        }
+        flag = false ;
+        for(int i=1;i<n;i++)
+        {
+        if(grid[0][i]==1||flag) { grid[0][i] = -1 ; flag = true ; }
+        else grid[0][i] = 1 ;
+        }
+        for(int i=1;i<m;i++)
+        {
+            for(int j=1;j<n;j++)
+            {
+                if(grid[i][j]==1) 
+                {
+                grid[i][j] = -1 ; continue ;
+                }
+                int x = grid[i-1][j]==-1?0:grid[i-1][j] ;
+                int y = grid[i][j-1]==-1?0:grid[i][j-1] ;
+                grid[i][j]= x + y ;
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print(grid[i][j] +" ") ;
+            }
+            System.out.println(" ") ;
+        }
+        return grid[m-1][n-1]==-1?0:grid[m-1][n-1] ;
+    }
+}
